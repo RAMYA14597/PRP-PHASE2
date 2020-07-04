b37987ae44cd6a37a8f53c37e006465ed04f92a1
RAMYA14597 <ramyaerusan@gmail.com>
2020-07-04 06:55:06 -0700

    Create rock_paper_scissor.java

---
+++ rock_paper_scissor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.*;
+public class rock_paper_scissor
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char P,R,S;
+char c1=sc.next().charAt(0);
+char c2=sc.next().charAt(0);
+if(c1=='P' && c2=='R')
+{
+System.out.println("P");
+}
+else if(c1=='P' && c2=='S')
+{
+System.out.println("S");
+}
+else
+{
+if(c1=='R' && c2=='S')
+{
+System.out.println("R");
+}
+}
+}}
